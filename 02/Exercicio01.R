e89b7c412f36d7a401672ea3624d64e4b6af4f25
arturmabrao <arturmabrao@gmail.com>
2022-08-01 16:56:02 -0300

    Exercicios

---
+++ 02/Exercicio01.R
@@ -0,0 +1,45 @@
+setwd("D:/Estudo/Tecnologias/BI/PowerBI/Cap12/R/ini-R/Data/1-Notas")
+getwd()
+
+# Carregamento dataset 
+notas <- read.csv("Notas.csv", fileEncoding = "windows-1252")
+
+# Exercicio 1
+View(notas)
+str(notas)
+summary(notas$TurmaA)
+summary(notas$TurmaB)
+
+# Exercicio 2
+mean(notas$TurmaA)
+mean(notas$TurmaB)
+
+# Exercicio 3
+sd(notas$TurmaA)
+sd(notas$TurmaB)
+
+
+# Exercicio 4
+calcCoefVar <- function(v){
+  cf_v <- sd(v) / mean(v) * 100
+  return(cf_v)
+}
+
+coef_var_A <- calcCoefVar(notas$TurmaA)
+coef_var_B <- calcCoefVar(notas$TurmaB)
+
+print(coef_var_A)
+print(coef_var_B)
+
+
+# Exercicio 5
+calcModa <- function(v) {
+  valor_unico <- unique(v)
+  valor_unico[which.max(tabulate(match(v, valor_unico)))]
+}
+
+moda_turmaA <- calcModa(notas$TurmaA)
+moda_turmaB <- calcModa(notas$TurmaB)
+
+print(moda_turmaA)
+print(moda_turmaB)
