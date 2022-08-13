68ebd4191cb2a77cd9d1270f1fa8b82d9009f0f0
arturmabrao <arturmabrao@gmail.com>
2022-08-13 11:42:53 -0300

    Ex02 T03

--- PBI/PowerBI_ex_02.R
+++ PBI/PowerBI_ex_02.R
@@ -33,3 +33,17 @@ dados_iris_id <- data.table(dados_iris)
 View(dados_iris_id)
 dados_iris_id$Sepal.Length <- as.integer(dados_iris_id$Sepal.Length)
 View(dados_iris_id)
+
+# Cores disponiveis em R:
+#colors()
+
+#T3: Construir um grafico mostando a relacao de duas variaveis num. para as 3 categorias de uma var categorica
+library(ggplot2)
+ggplot(data = dados_iris, aes(x = Petal.Width, y = Petal.Length))+
+  geom_point(aes(color = Species), size = 3)+
+  ggtitle("Largura e Comprimento das Petalas")+
+  labs(x = "Largura da Petala",
+       y = "Comprimento da Petala")+
+  theme_bw()+
+  theme(title = element_text(size = 15, color = "turquoise4"))
+
